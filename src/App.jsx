a64288535ab1e8f12538f22c67192fc76d2c128b
Allen <allenretuta10@yahoo.com>
2025-12-10 10:11:57 +0800

    fixed basename

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename='/portfolio-v2'>
         <Routes>
           <Route index element={<HomePage />} />
           <Route path='*' element={<NotFound />} />
